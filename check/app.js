850aa587b1c2e53b2453910f22ad2324b5e49ebb
2909638069 <2909638069@qq.com>
2019-07-01 18:53:41 +0800

    update

--- check/app.js
+++ check/app.js
@@ -3,6 +3,8 @@ let express = require('express');
 let multer = require('multer');
 let child_process = require('child_process');
 let path = require('path');
+let mysql = require('mysql');
+
 
 let app = express();
 let createFolder = (folder) => {
@@ -13,23 +15,30 @@ let createFolder = (folder) => {
   fs.mkdirSync(folder);
   }
 };
-let tmpFolder = './tmp/';  // only one file. the lastest version
-createFolder(tmpFolder);
-let uploadFolder = './upload/';  // history versions.
-createFolder(uploadFolder);
-let downloadFolder = './download/';  // patches
-createFolder(downloadFolder);
-
+let latestFolder = './latest/';  // only one file. the lastest version
+createFolder(latestFolder);
+let pastsFolder = './pasts/';  // history versions.
+createFolder(pastsFolder);
+let patchesFolder = './patches/';  // patches
+createFolder(patchesFolder);
 
 let storage = multer.diskStorage(
-  { destination: (req, file, cb) => { cb(null, tmpFolder); },
+  { destination: (req, file, cb) => { cb(null, latestFolder); },
   filename: (req, file, cb) => { cb(null, file.originalname); } }
 );
 let upload = multer({ storage: storage });
 
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'dyw',
+  port: '3306',
+});
+
 
 app.get('/up', (req, res, next) => {
-  child_process.exec('mv tmp/* upload/');  // move  the lastest version to history version
+  child_process.exec('mv latest/* pasts/');  // move  the lastest version to pasts version
   let form = fs.readFileSync('./index.html', { encoding: 'utf8' });
   res.send(form);
 });
@@ -45,19 +54,20 @@ app.post('/upload', upload.single('logo'), (req, res, next) => {
   res.send(JSON.stringify(fileInfo));
   
   child_process.exec(
-  'for file in `ls ./upload/`; do bsdiff ./upload/$file ./tmp/* download/${file}$(ls ./tmp/); done;'
-
-
-
+  'for file in `ls ./pasts/`; do bsdiff ./pasts/$file ./latest/* patches/${file}$(ls ./latest/); done;'
   );
+  latest = fs.readdirSync('./latest/');
+  pasts = fs.readdirSync('./pasts/');
+  console.log(pasts);
+  console.log(latest);
 });
 
 
 app.get('/down', (req, res, next) => {
   let name = req.query.name;
-  let latest = fs.readdirSync('tmp');
+  let latest = fs.readdirSync('latest');
   let queryfile = name + latest;
-  var queryfilepath = path.resolve(__dirname, 'download/', queryfile);
+  var queryfilepath = path.resolve(__dirname, 'patches/', queryfile);
   res.download(queryfilepath);
 })
 
